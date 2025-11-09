da94fa1c7931c3cbc8a1b02956aedac6603428f3
JanaAlshomrani <Janaalshomrani2@gmail.com>
2025-11-09 22:05:36 +0300

    Add files via upload

---
+++ Room.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Room {
+	//Jana: I think a class named room is better the minigame since we can creat the 4 
+	//rooms here and use a method like room1.play() or room1.show() or something
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
