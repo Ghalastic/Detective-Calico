8503f34e4849eb6c5ed1ea76d7c8359a0b128445
RaghadEE23 <ragadbalamsh@gmail.com>
2025-11-24 16:59:48 +0300

    ThirdRoom.java

--- ThirdRoom.java
+++ ThirdRoom.java
@@ -1,9 +1,5 @@
 package javaProject;
-import javax.swing.BorderFactory;
-
-import javaProject.Sudoku.Tile;
 import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,52 +7,71 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
 
 public class ThirdRoom extends Application {
 	 private Button numSelected=null;
 	 private int score=0;
     public void start(Stage firstPage) {
     	firstPage.setTitle("Sudoku");
-		firstPage.setResizable(false);
+    	firstPage.setFullScreenExitHint("");
+    	firstPage.setFullScreen(true);
+
+    	Pane background=new Pane();
+    	background.setStyle(
+    		    "-fx-background-image: url('/images/thirdroom.png'); " +
+    		    "-fx-background-size: cover;"
+    		);
+    	
+        ImageView cat = new ImageView("/images/walkingright2.png");
+    	cat.setFitWidth(150);
+    	cat.setFitHeight(200);
+    	cat.setLayoutX(400);   // موقع الصورة
+    	cat.setLayoutY(500);
+    	background.getChildren().add(cat);
+    	
+    
     	Pane pane=new Pane();
-    	Scene scene1=new Scene(pane,600,650,Color.RED);
     	
     	Label title=new Label("Welcome to the third room");
-    	title.setLayoutX(115);
+    	title.setLayoutX(500);
     	title.setLayoutY(80);
     	title.setFont(Font.font("Centry gothic",FontWeight.BOLD,30));
+    	title.setTextFill(Color.WHITE);
     	pane.getChildren().add(title);
     	
     	Label rule=new Label("In this room you have to solve Sudoku.You have a 9×9 grid and \n a set of numbers 1 to 9 at the bottom."
     			     		  +"To play, pick a number\n(1–9) from the bottom, then click on any empty cell in the grid\n     to place it."+
     			      		  " Remember the rule: a number must not repeat\n       in the same row, the same colum, or the same 3×3 box.");
-    	rule.setLayoutX(20); 
+    	rule.setLayoutX(400); 
     	rule.setLayoutY(250);
     	rule.setFont(new Font("Centry gothic",20));
-    	rule.setTextFill(Color.BLACK);
+    	rule.setTextFill(Color.WHITE);
     	pane.getChildren().add(rule);
     	
     	
     	Button b=new Button("Start");
-    	b.setLayoutX(280);
-    	b.setLayoutY(500);
+    	b.setLayoutX(670);
+    	b.setLayoutY(460);
     	b.setFont(new Font("Arail",15));
     	pane.getChildren().add(b);
+    	
+    	
+    	
     	final String[] puzzle = {
-	          "--74916-5",
-	          "2---6-3-9",
-	          "-----7-1-",
-	          "-586----4",
-	          "--3----9-",
-	          "--62--187",
-	          "9-4-7---2",
-	          "67-83----",
-	          "81--45---"
+   		        "387491625",
+   		        "241568379",
+   		        "569327418",
+   		        "758619234",
+   		        "123784596",
+   		        "496253187",
+   		        "934176852",
+   		        "675832941",
+   		        "81--45---"
    		    };
    	final String[] solution = {
    		        "387491625",
@@ -71,18 +86,17 @@ public class ThirdRoom extends Application {
    		    };
     	
     	b.addEventHandler(MouseEvent.MOUSE_PRESSED, (MouseEvent e)->{
-    	   firstPage.hide();
+    	  
     
     	   Pane pane2=new Pane();
-    	   Scene scene2=new Scene(pane2,600,650);
     	   
     	   Label scoreText=new Label("Score: 0");
     	   scoreText.setFont(Font.font("Arial",FontWeight.BOLD,20));
-    	   scoreText.setLayoutX(255);
+    	   scoreText.setTextFill(Color.WHITE);
+    	   scoreText.setLayoutX(660);
     	   scoreText.setLayoutY(20);
     	   pane2.getChildren().add(scoreText);
     	   
-    	  
     	   
     	   GridPane grid=new GridPane();
     	   for(int r=0;r<9;r++) {
@@ -131,21 +145,44 @@ public class ThirdRoom extends Application {
     	    					score++;
     	    					scoreText.setText("Score= "+String.valueOf(score));
     	    				}}
-    					  if(score==46) {
-    			       		   showWinScene(firstPage);
+    					  if(score==5) {
+    						  Pane pane3=new Pane();
+   						   
+   						   Label Hint3=new Label("\t      Great goob\n Hint3: ");
+   						   Hint3.setFont(Font.font("Centry gothic",FontWeight.BOLD,30));
+   						   Hint3.setTextFill(Color.WHITE);
+   						   Hint3.setLayoutX(500);
+   						   Hint3.setLayoutY(200);
+   						   pane3.getChildren().add(Hint3);
+   						   
+   						   
+   						   Button Home=new Button("Home");
+   						   Home.setLayoutX(670);
+   					       Home.setLayoutY(460);
+   					       Home.setFont(new Font("Arail",15));
+   						   pane3.getChildren().add(Home);
+   						   
+   						   StackPane root3 = new StackPane();
+   				           root3.getChildren().add(background);
+   				           root3.getChildren().add(pane3);
+   				           
+   				           Scene scene3 = new Scene(root3, 800, 650);
+   				           firstPage.setScene(scene3);
+   				           firstPage.show();
+   				           firstPage.setFullScreenExitHint("");
+   				           firstPage.setFullScreen(true);
     			    		  }
     					  
     				  });
     				 
     		   }
-        	   
-        		   
+        	      
         	   }
     	   
           
     	   grid.setVgap(0.5);
     	   grid.setHgap(0.5);
-    	   grid.setLayoutX(60);
+    	   grid.setLayoutX(460);
 		   grid.setLayoutY(60);
     	   pane2.getChildren().add(grid);
     	   
@@ -161,46 +198,39 @@ public class ThirdRoom extends Application {
     	   }
     	 
     		
-    	   grid2.setHgap(5);
-    	   grid2.setLayoutX(50);
-		   grid2.setLayoutY(580);
-    	   pane2.getChildren().add(grid2);
-    	   
+    	    grid2.setHgap(5);
+    	    grid2.setLayoutX(450);
+		    grid2.setLayoutY(580);
+    	    pane2.getChildren().add(grid2);
     	  
     	   
-    	   firstPage.setScene(scene2);
-    	   firstPage.show();
-    	   
-   
+    	    StackPane root2 = new StackPane();
+            root2.getChildren().add(background);
+            root2.getChildren().add(pane2);
+
+            Scene scene2 = new Scene(root2, 800, 650);
+        
+        	firstPage.setScene(scene2);
+        	firstPage.setFullScreenExitHint("");
+        	firstPage.setFullScreen(true);
+         	firstPage.show();
+       	
     	});
     	
     	
-    	  
-    	firstPage.setScene(scene1);
+    	StackPane root = new StackPane();
+        root.getChildren().add(background);
+        root.getChildren().add(pane);
+
+        Scene scene = new Scene(root, 800, 650);
+    	
+    	firstPage.setScene(scene);
     	firstPage.show();
+    	
     
     	
     }
-private void showWinScene(Stage stage) {
-	   stage.hide();
-	   Pane pane3=new Pane();
-	   Scene sceneWin=new Scene(pane3,600,600);
-	   Label Hint3=new Label("\t    Great goob\n Hint3: ");
-	   Hint3.setFont(Font.font("Centry gothic",FontWeight.BOLD,30));
-	   Hint3.setLayoutX(100);
-	   Hint3.setLayoutY(200);
-	   pane3.getChildren().add(Hint3);
-	   
-	   
-	   Button Home=new Button("Home");
-	   Home.setLayoutX(260);
-       Home.setLayoutY(400);
-       Home.setFont(new Font("Arail",15));
-	   pane3.getChildren().add(Home);
-	   
-	   stage.setScene(sceneWin);
-	   stage.show();
-}
+
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
     launch(args);
